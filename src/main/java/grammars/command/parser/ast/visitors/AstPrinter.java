1eda11c85420a661836b7f40d7d222e6fd5ad652
mattcce <ce.mattheus.cheong@gmail.com>
2025-09-18 01:25:22 +0800

    feat: Added pretty-printer for command ASTs

--- src/main/java/grammars/command/parser/ast/visitors/AstPrinter.java
+++ src/main/java/grammars/command/parser/ast/visitors/AstPrinter.java
@@ -1,52 +1,144 @@
-package grammars.command.parser.ast;
+package grammars.command.parser.ast.visitors;
 
-public class AstPrinter extends AstVisitor<String> {
-    @Override
-    public String visitCommand() {
+import java.util.ArrayList;
+import java.util.List;
+
+import grammars.command.parser.ast.AstNode;
+import grammars.command.parser.ast.AstVisitor;
+
+/**
+ * Traverses an AST and builds a formatted string representation.
+ */
+public class AstPrinter implements AstVisitor<String> {
+    /**
+     * Public entry point to print any AST node.
+     */
+    public String print(AstNode node) {
+        return node.accept(this);
     }
 
     @Override
-    public String visitImperative() {
-        return "";
+    public String visitCommand(AstNode.Command node) {
+        StringBuilder builder = new StringBuilder("Command");
+
+        List<AstNode> children = new ArrayList<>();
+        children.add(node.getImperative());
+        if (node.getParameterList() != null) {
+            children.add(node.getParameterList());
+        }
+        if (node.getOptionList() != null) {
+            children.add(node.getOptionList());
+        }
+
+        for (int i = 0; i < children.size(); i++) {
+            boolean isLast = (i == children.size() - 1);
+            String childString = children.get(i).accept(this);
+            builder.append("\n").append(addPrefixes(childString, isLast));
+        }
+        return builder.toString();
+    }
+
+    /**
+     * Adds the correct tree-like prefixes to each line of a child's output.
+     *
+     * @param childOutput The fully-rendered string of a child node.
+     * @param isLast      True if this is the last child of its parent.
+     * @return The prefixed string.
+     */
+    private String addPrefixes(String childOutput, boolean isLast) {
+        StringBuilder builder = new StringBuilder();
+        String[] lines = childOutput.split("\n");
+
+        String firstLinePrefix = isLast ? "└─ " : "├─ ";
+        builder.append(firstLinePrefix).append(lines[0]);
+
+        String restLinesPrefix = isLast ? "   " : "│  ";
+        for (int i = 1; i < lines.length; i++) {
+            builder.append("\n").append(restLinesPrefix).append(lines[i]);
+        }
+        return builder.toString();
     }
 
     @Override
-    public String visitParameterList() {
-        return "";
+    public String visitImperative(AstNode.Imperative node) {
+        StringBuilder builder = new StringBuilder("Imperative");
+        String wordString = node.getWord().accept(this);
+        builder.append("\n").append(addPrefixes(wordString, true)); // Always the last/only child
+        return builder.toString();
     }
 
     @Override
-    public String visitParameter() {
-        return "";
+    public String visitParameterList(AstNode.ParameterList node) {
+        StringBuilder builder = new StringBuilder("ParameterList");
+        List<AstNode.Parameter> params = node.getParameters();
+        for (int i = 0; i < params.size(); i++) {
+            boolean isLast = (i == params.size() - 1);
+            String paramString = params.get(i).accept(this);
+            builder.append("\n").append(addPrefixes(paramString, isLast));
+        }
+        return builder.toString();
     }
 
     @Override
-    public String visitOptionList() {
-        return "";
+    public String visitParameter(AstNode.Parameter node) {
+        StringBuilder builder = new StringBuilder("Parameter");
+        String wordString = node.getWord().accept(this);
+        builder.append("\n").append(addPrefixes(wordString, true));
+        return builder.toString();
     }
 
     @Override
-    public String visitOption() {
-        return "";
+    public String visitOptionList(AstNode.OptionList node) {
+        StringBuilder builder = new StringBuilder("OptionList");
+        List<AstNode.Option> options = node.getOptions();
+        for (int i = 0; i < options.size(); i++) {
+            boolean isLast = (i == options.size() - 1);
+            String optionString = options.get(i).accept(this);
+            builder.append("\n").append(addPrefixes(optionString, isLast));
+        }
+        return builder.toString();
     }
 
     @Override
-    public String visitOptionName() {
-        return "";
+    public String visitOption(AstNode.Option node) {
+        StringBuilder builder = new StringBuilder("Option");
+        List<AstNode> children = new ArrayList<>();
+        children.add(node.getOptionName());
+        if (node.getOptionValue() != null) {
+            children.add(node.getOptionValue());
+        }
+
+        for (int i = 0; i < children.size(); i++) {
+            boolean isLast = (i == children.size() - 1);
+            String childString = children.get(i).accept(this);
+            builder.append("\n").append(addPrefixes(childString, isLast));
+        }
+        return builder.toString();
     }
 
     @Override
-    public String visitOptionValue() {
-        return "";
+    public String visitOptionName(AstNode.OptionName node) {
+        StringBuilder builder = new StringBuilder("OptionName");
+        String wordString = node.getWord().accept(this);
+        builder.append("\n").append(addPrefixes(wordString, true));
+        return builder.toString();
     }
 
     @Override
-    public String visitText() {
-        return "";
+    public String visitOptionValue(AstNode.OptionValue node) {
+        StringBuilder builder = new StringBuilder("OptionValue");
+        String textString = node.getText().accept(this);
+        builder.append("\n").append(addPrefixes(textString, true));
+        return builder.toString();
     }
 
     @Override
-    public String visitWord() {
-        return "";
+    public String visitText(AstNode.Text node) {
+        return "Text (\"" + node.getText() + "\")";
+    }
+
+    @Override
+    public String visitWord(AstNode.Word node) {
+        return "Word (\"" + node.getWord() + "\")";
     }
 }
